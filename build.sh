08813a65572069ff6cadb1fddc400c7043013cc4
Kevin Edwards <kedwards@kevinedwards.ca>
2019-02-27 08:54:51 -0700

    reorganized repository.

--- build.sh
+++ build.sh
@@ -26,9 +26,8 @@ docker run --rm -itd \
 --name jenkins \
 --env JAVA_OPTS=-Dhudson.footerURL=http://kevinedwards.ca \
 -p 8080:8080 \
--p 50000:50000 \
--v /vagrant/jenkins_home:/var/jenkins_home \
 -v /var/run/docker.sock:/var/run/docker.sock \
+-v /vagrant/jenkins_home:/var/jenkins_home \
 kevinedwards/jenkins-master:latest \
 "\$@"
 EOF
